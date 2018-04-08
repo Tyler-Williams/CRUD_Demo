b4aa037266c5a1bcbbde098a5b995e7b8b3efd66
Tyler-Williams <github@tylerclarkwilliams.com>
2018-04-07 23:42:25 -0700

    initial commit, /posts route complete

---
+++ client/src/components/Posts.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+    <div v-for="post in posts" :key="post.title">
+      <p>
+        <span><b>{{ post.title }}</b></span>
+        <span>{{ post.description }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostsService from '@/services/PostsService'
+
+export default {
+  name: 'posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
