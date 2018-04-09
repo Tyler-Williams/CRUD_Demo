e2928228d95dbad595beb15d10a12abbcbed04f4
Tyler-Williams <github@tylerclarkwilliams.com>
2018-04-08 21:21:44 -0700

    CRUD Demo complete

--- server/src/app.js
+++ server/src/app.js
@@ -30,6 +30,14 @@ app.get('/posts', (req, res) => {
   }).sort({_id: -1})
 })
 
+//  GET single item REQ
+app.get('/posts/:id', (req, res) => {
+  Post.findById(req.params.id, 'title description', (err, post) => {
+    if (err) { console.error(err) }
+    res.send(post)
+  })
+})
+
 //  POST REQ
 app.post('/posts', (req, res) => {
   const title = req.body.title
@@ -47,6 +55,35 @@ app.post('/posts', (req, res) => {
     res.send({
       success: true,
       message: 'Post saved sccessfully'
+    })
+  })
+})
+
+//  PUT REQ
+app.put('/posts/:id', (req, res) => {
+  Post.findById(req.params.id, 'title description', (err, post) => {
+    if (err) { console.error(err) }
+
+    post.title = req.body.title,
+    post.description = req.body.description
+
+    post.save((err) => {
+      if (err) { console.error(err) }
+      res.send({
+        success: true
+      })
+    })
+  })
+})
+
+//  DELETE REQ
+app.delete('/posts/:id', (req, res) => {
+  Post.remove({
+    _id: req.params.id
+  }, (err, post) => {
+    if (err) { res.send(err) }
+    res.send({
+      success: true
     })
   })
 })
